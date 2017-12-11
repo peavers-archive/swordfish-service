57ac4c706ee7abf2ee6d3018483ebfd72fec5092
Chris Turner <peavers@gmail.com>
2017-12-11 16:12:48 +1300

    * removed constructor from notification configuration as not required

--- instance-service/src/main/java/space/swordfish/instance/service/configuration/NotificationConfig.java
+++ instance-service/src/main/java/space/swordfish/instance/service/configuration/NotificationConfig.java
@@ -1,28 +1,15 @@
 package space.swordfish.instance.service.configuration;
 
-import com.amazonaws.services.sqs.AmazonSQSAsync;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import space.swordfish.common.json.services.JsonTransformService;
 import space.swordfish.common.notification.services.NotificationServiceImpl;
 
 @Configuration
 public class NotificationConfig {
 
-    @Autowired
-    private QueueMessagingTemplate queueMessagingTemplate;
-
-    @Autowired
-    private JsonTransformService jsonTransformService;
-
-    @Autowired
-    private AmazonSQSAsync amazonSQSAsync;
-
     @Bean
     public NotificationServiceImpl notificationService() {
-        return new NotificationServiceImpl(queueMessagingTemplate, amazonSQSAsync, (space.swordfish.common.notification.services.JsonTransformService) jsonTransformService);
+        return new NotificationServiceImpl();
     }
 
 }
