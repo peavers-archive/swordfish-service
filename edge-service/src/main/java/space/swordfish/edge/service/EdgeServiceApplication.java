961384f90253fce6ef963940a94c1d7956d7433e
Chris Turner <peavers@gmail.com>
2017-12-12 16:17:03 +1300

    * testing more docker-swarm stuff;

--- edge-service/src/main/java/space/swordfish/edge/service/EdgeServiceApplication.java
+++ edge-service/src/main/java/space/swordfish/edge/service/EdgeServiceApplication.java
@@ -2,15 +2,12 @@ package space.swordfish.edge.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
-
 import space.swordfish.edge.service.filter.AuthorizationFilter;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableZuulProxy
 @EnableHystrix
 public class EdgeServiceApplication {
