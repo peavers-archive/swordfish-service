effc78f555d52e799a59af1840204b7d51eb0895
Chris Turner <peavers@gmail.com>
2017-12-12 13:51:02 +1300

    * applying fix for other services

--- instance-service/src/main/java/space/swordfish/instance/service/InstanceServiceApplication.java
+++ instance-service/src/main/java/space/swordfish/instance/service/InstanceServiceApplication.java
@@ -1,14 +1,8 @@
 package space.swordfish.instance.service;
 
-import com.netflix.appinfo.AmazonInfo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan({ "space.swordfish.common.*" })
@@ -16,23 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class InstanceServiceApplication {
 
-	@Value("${server.port:8761}")
-	private int port;
-
 	public static void main(String[] args) {
 		SpringApplication.run(InstanceServiceApplication.class, args);
 	}
-
-	@Bean
-	@Autowired
-	public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils inetUtils) {
-		EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
-		AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
-		config.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
-		config.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
-		config.setNonSecurePort(port);
-		config.setDataCenterInfo(info);
-		return config;
-	}
-
 }
