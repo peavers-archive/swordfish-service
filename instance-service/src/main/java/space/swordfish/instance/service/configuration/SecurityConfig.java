643e76eb23ef099cf5082cd83c23c5f4c6ef1043
Chris Turner <peavers@gmail.com>
2017-12-13 16:10:27 +1300

    * spring consul still blocks JWTs

--- instance-service/src/main/java/space/swordfish/instance/service/configuration/SecurityConfig.java
+++ instance-service/src/main/java/space/swordfish/instance/service/configuration/SecurityConfig.java
@@ -1,27 +1,35 @@
 package space.swordfish.instance.service.configuration;
 
 import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.ManagementServerProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
-@Order(1)
+@Order(ManagementServerProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Value(value = "${auth0.audience}")
-	private String audience;
+    @Value(value = "${auth0.audience}")
+    private String audience;
 
-	@Value(value = "${auth0.issuer}")
-	private String issuer;
+    @Value(value = "${auth0.issuer}")
+    private String issuer;
 
-	@Override
-	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		JwtWebSecurityConfigurer.forRS256(audience, issuer).configure(httpSecurity).csrf()
-				.disable().authorizeRequests().anyRequest().authenticated();
-	}
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.NEVER);
+
+        JwtWebSecurityConfigurer.forRS256(audience, issuer).configure(httpSecurity)
+                .csrf().disable()
+                .authorizeRequests().anyRequest().authenticated();
+    }
 }
