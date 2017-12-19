86b40dc0da070c92241f4b3787cb5014d4a90eaa
Chris Turner <peavers@gmail.com>
2017-12-19 17:10:02 +1300

    * debugging in production, my most favorite place :)

--- instance-service/src/main/java/space/swordfish/instance/service/controller/InstanceController.java
+++ instance-service/src/main/java/space/swordfish/instance/service/controller/InstanceController.java
@@ -1,11 +1,12 @@
 package space.swordfish.instance.service.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.*;
+
+import lombok.extern.slf4j.Slf4j;
 import space.swordfish.common.json.services.JsonTransformService;
 import space.swordfish.instance.service.domain.Instance;
 import space.swordfish.instance.service.repository.InstanceRepository;
@@ -15,48 +16,50 @@ import space.swordfish.instance.service.repository.InstanceRepository;
 @RequestMapping("/instances")
 public class InstanceController {
 
-    @Autowired
-    private InstanceRepository instanceRepository;
+	@Autowired
+	private InstanceRepository instanceRepository;
 
-    @Autowired
-    private JsonTransformService jsonTransformService;
+	@Autowired
+	private JsonTransformService jsonTransformService;
 
-    @Autowired
-    private QueueMessagingTemplate queueMessagingTemplate;
+	@Autowired
+	private QueueMessagingTemplate queueMessagingTemplate;
 
-    @Value("${queues.instanceEvents}")
-    private String queue;
+	@Value("${queues.instanceEvents}")
+	private String queue;
 
-    @GetMapping()
-    public String findAll() {
-        Iterable<Instance> instances = instanceRepository.findAll();
+	@GetMapping()
+	public String findAll() {
+		Iterable<Instance> instances = instanceRepository.findAll();
 
-        return jsonTransformService.writeList(instances);
-    }
+		log.info("Returning instances {}", instances);
 
-    @GetMapping("{id}")
-    public String findById(@PathVariable String id) {
-        Instance instance = instanceRepository.findById(id);
+		return jsonTransformService.writeList(instances);
+	}
 
-        return jsonTransformService.write(instance);
-    }
+	@GetMapping("{id}")
+	public String findById(@PathVariable String id) {
+		Instance instance = instanceRepository.findById(id);
 
-    @PostMapping()
-    public String post(@RequestBody String payload) {
-        this.queueMessagingTemplate.send(queue,
-                MessageBuilder.withPayload(payload).build());
-        Instance instance = jsonTransformService.read(Instance.class, payload);
+		return jsonTransformService.write(instance);
+	}
 
-        return jsonTransformService.write(instance);
-    }
+	@PostMapping()
+	public String post(@RequestBody String payload) {
+		this.queueMessagingTemplate.send(queue,
+				MessageBuilder.withPayload(payload).build());
+		Instance instance = jsonTransformService.read(Instance.class, payload);
 
-    @PatchMapping("{id}")
-    @DeleteMapping("{id}")
-    public String patch(@RequestBody String payload, @PathVariable("id") String id) {
-        this.queueMessagingTemplate.send(queue,
-                MessageBuilder.withPayload(payload).build());
-        Instance instance = jsonTransformService.read(Instance.class, payload);
+		return jsonTransformService.write(instance);
+	}
 
-        return jsonTransformService.write(instance);
-    }
+	@PatchMapping("{id}")
+	@DeleteMapping("{id}")
+	public String patch(@RequestBody String payload, @PathVariable("id") String id) {
+		this.queueMessagingTemplate.send(queue,
+				MessageBuilder.withPayload(payload).build());
+		Instance instance = jsonTransformService.read(Instance.class, payload);
+
+		return jsonTransformService.write(instance);
+	}
 }
