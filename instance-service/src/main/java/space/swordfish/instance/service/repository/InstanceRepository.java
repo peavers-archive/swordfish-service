debcffd6fc2d913e3c080b69dcb048767f8a0110
Chris Turner <peavers@gmail.com>
2017-12-24 10:01:13 +1300

    * renamed projected to swordfish-service * set to only shutdown instances that are managed by swordfish, and have their production flags set to false

--- instance-service/src/main/java/space/swordfish/instance/service/repository/InstanceRepository.java
+++ instance-service/src/main/java/space/swordfish/instance/service/repository/InstanceRepository.java
@@ -2,21 +2,22 @@ package space.swordfish.instance.service.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
 import space.swordfish.instance.service.domain.Instance;
 
 @RepositoryRestResource
 public interface InstanceRepository extends CrudRepository<Instance, String> {
 
-	Instance findByInstanceId(String instanceId);
+    Instance findByInstanceId(String instanceId);
 
-	Iterable<Instance> findAllByStateAndProduction(String state, boolean production);
+    Iterable<Instance> findAllByStateAndProduction(String state, boolean production);
 
-	Instance findById(String id);
+    Iterable<Instance> findAllByStateAndSwordfishIsTrueAndProductionIsFalse(String state);
 
-	void deleteByInstanceId(String instanceId);
+    Instance findById(String id);
 
-	Iterable<Instance> findAllByState(String terminated);
+    void deleteByInstanceId(String instanceId);
 
-	Iterable<Instance> findAllByUserId(String userId);
+    Iterable<Instance> findAllByState(String terminated);
+
+    Iterable<Instance> findAllByUserId(String userId);
 }
--- instance-service/src/main/java/space/swordfish/instance/service/task/InstanceShutdownTask.java
+++ instance-service/src/main/java/space/swordfish/instance/service/task/InstanceShutdownTask.java
@@ -1,58 +1,57 @@
 package space.swordfish.instance.service.task;
 
-import javax.annotation.PostConstruct;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Component;
-
-import lombok.extern.slf4j.Slf4j;
 import space.swordfish.instance.service.domain.Instance;
 import space.swordfish.instance.service.repository.InstanceRepository;
 import space.swordfish.instance.service.service.EC2Stop;
+
+import javax.annotation.PostConstruct;
 
 @Slf4j
 @Component
 public class InstanceShutdownTask {
 
-	private final EC2Stop ec2Stop;
-	private final CronTrigger cronTrigger;
-	private final InstanceRepository instanceRepository;
-	private final ThreadPoolTaskScheduler taskScheduler;
+    private final EC2Stop ec2Stop;
+    private final CronTrigger cronTrigger;
+    private final InstanceRepository instanceRepository;
+    private final ThreadPoolTaskScheduler taskScheduler;
 
-	@Autowired
-	public InstanceShutdownTask(
-			@Qualifier("threadPoolTaskScheduler") ThreadPoolTaskScheduler taskScheduler,
-			CronTrigger cronTrigger, InstanceRepository instanceRepository,
-			EC2Stop ec2Stop) {
-		this.taskScheduler = taskScheduler;
-		this.cronTrigger = cronTrigger;
-		this.instanceRepository = instanceRepository;
-		this.ec2Stop = ec2Stop;
-	}
+    @Autowired
+    public InstanceShutdownTask(
+            @Qualifier("threadPoolTaskScheduler") ThreadPoolTaskScheduler taskScheduler,
+            CronTrigger cronTrigger, InstanceRepository instanceRepository,
+            EC2Stop ec2Stop) {
+        this.taskScheduler = taskScheduler;
+        this.cronTrigger = cronTrigger;
+        this.instanceRepository = instanceRepository;
+        this.ec2Stop = ec2Stop;
+    }
 
-	@PostConstruct
-	public void scheduleRunnableWithCronTrigger() {
-		taskScheduler.schedule(new RunnableTask(), cronTrigger);
-	}
+    @PostConstruct
+    public void scheduleRunnableWithCronTrigger() {
+        taskScheduler.schedule(new RunnableTask(), cronTrigger);
+    }
 
-	/**
-	 * Thread for shutting down servers when the cron-trigger stipulates
-	 */
-	class RunnableTask implements Runnable {
+    /**
+     * Thread for shutting down servers when the cron-trigger stipulates
+     */
+    class RunnableTask implements Runnable {
 
-		@Override
-		public void run() {
-			Iterable<Instance> instances = instanceRepository
-					.findAllByStateAndProduction("running", false);
+        @Override
+        public void run() {
+            Iterable<Instance> instances = instanceRepository
+                    .findAllByStateAndSwordfishIsTrueAndProductionIsFalse("running");
 
-			for (Instance instance : instances) {
-				log.info("automatically shutting down {}", instance.getTags());
+            for (Instance instance : instances) {
+                log.info("automatically shutting down {}", instance.getTags());
 
-				ec2Stop.stop(instance.getInstanceId());
-			}
-		}
-	}
+                ec2Stop.stop(instance.getInstanceId());
+            }
+        }
+    }
 }
