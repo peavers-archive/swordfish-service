2b6457857a0c3f580294f31d8b0a0530081b1a5b
Chris Turner <peavers@gmail.com>
2017-12-31 10:46:17 +1300

    * node doesn't need eureka either

--- node-service/src/main/java/space/swordfish/node/service/NodeServiceApplication.java
+++ node-service/src/main/java/space/swordfish/node/service/NodeServiceApplication.java
@@ -2,9 +2,7 @@ package space.swordfish.node.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class NodeServiceApplication {
 
