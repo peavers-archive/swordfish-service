9ea94b7a7230f7b51d02e7fa6abcd405b43a5d85
Chris Turner <peavers@gmail.com>
2018-01-02 17:08:36 +1300

    * code style fix

--- notification-service/src/main/java/space/swordfish/notification/service/services/PusherServiceImpl.java
+++ notification-service/src/main/java/space/swordfish/notification/service/services/PusherServiceImpl.java
@@ -7,15 +7,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class PusherServiceImpl implements PusherService {
 
-	private final Pusher pusher;
+    @Autowired
+    private Pusher pusher;
 
-	@Autowired
-	public PusherServiceImpl(Pusher pusher) {
-		this.pusher = pusher;
-	}
-
-	@Override
-	public void push(String channel, String event, String message) {
-		pusher.trigger(channel, event, message);
-	}
+    @Override
+    public void push(String channel, String event, String message) {
+        pusher.trigger(channel, event, message);
+    }
 }
