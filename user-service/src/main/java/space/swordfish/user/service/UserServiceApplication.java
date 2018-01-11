67968bf37417bf241479f41ed06c8ad6c9a68c88
Chris Turner <peavers@gmail.com>
2018-01-11 15:13:35 +1300

    * build the new user-service with Travis;

--- user-service/src/main/java/space/swordfish/user/service/UserServiceApplication.java
+++ user-service/src/main/java/space/swordfish/user/service/UserServiceApplication.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class UserServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
